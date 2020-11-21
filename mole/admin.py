36f6223e88b197fe1cfbe0374004304af702aff9
Johannes Wanner <johannes.wanner@web.de>
2020-11-21 09:37:33 +0100

    heroku test conf

--- mole/admin.py
+++ mole/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Event, Evidence
+#from .models import Event, Evidence
 
-admin.site.register(Event)
-admin.site.register(Evidence)
+#admin.site.register(Event)
+#admin.site.register(Evidence)

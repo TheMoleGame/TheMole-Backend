32b5e2d2fd47064c1323dbba5eb8ca20a8cc4ec0
Johannes Wanner <johannes.wanner@web.de>
2020-11-21 07:49:51 +0100

    registered the models to the admin ui

--- mole/admin.py
+++ mole/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Event, Evidence
+
+admin.site.register(Event)
+admin.site.register(Evidence)

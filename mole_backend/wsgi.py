f51e6fb5512c0af9322a33907d3c144db79e68f8
s0572407 <shari-lynn.eichberger@student.htw-berlin.de>
2020-12-04 10:33:36 +0100

    adjusted import

--- mole_backend/wsgi.py
+++ mole_backend/wsgi.py
@@ -12,9 +12,9 @@ import socketio
 
 from django.core.wsgi import get_wsgi_application
 
-from mole.views import sio
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mole_backend.settings")
+
+from mole.views import sio
 
 django_app = get_wsgi_application()
 application = socketio.WSGIApp(sio, django_app)

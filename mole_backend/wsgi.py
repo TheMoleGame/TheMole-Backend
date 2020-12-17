bdb7d8d1337b160822479390ff8aecc38191ec42
s0572407 <shari-lynn.eichberger@student.htw-berlin.de>
2020-12-17 18:40:24 +0100

    Test

--- mole_backend/wsgi.py
+++ mole_backend/wsgi.py
@@ -18,8 +18,3 @@ django_app = get_wsgi_application()
 from mole.views import sio
 
 application = socketio.WSGIApp(sio, django_app)
-
-from mole.db_init import *
-
-# Initialize the database the first time
-db_init()
